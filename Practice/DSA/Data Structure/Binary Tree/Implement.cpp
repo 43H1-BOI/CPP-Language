2eeb5e166fb7a2537225ba22e354c52dbadcd979
Abhishek Yadav <153166442+43H1-BOI@users.noreply.github.com>
2024-09-30 22:08:38 +0530

    Added Preorder , Postorder , Inorder ( Working )

--- Practice/DSA/Data Structure/Binary Tree/Implement.cpp
+++ Practice/DSA/Data Structure/Binary Tree/Implement.cpp
@@ -10,8 +10,8 @@ public:
 
     Node(int val) {
         data = val;
-        left = 0; // NULL
-        right = 0; // NULL
+        left = NULL;
+        right = NULL;
         count++;
     }
 
@@ -22,80 +22,47 @@ public:
 
 int Node::count = 0;
 
-class BT { // Binary Tree
-// private:
-public:
-    Node* root;
-
-    BT( ) {
-        root = 0; // NULL
-    }
-
-/*
-    void addLeft(int val , int data){
-        Node* newNode = new Node(val);
-        if(root){ // Empty Tree
-            root = newNode;
-        } else {
-            Node* temp = root;
-            while(temp->left != 0){
-
-            }
-
-        }
-
-    // void preorder(Node*& root); // Preorder : Root Left Right
-    // void postorder(Node*& root); // Postorder : Left Right Root
-    // void inorder(Node*& root); // Inorder : Left Root Right
-
-};
-
-
-void preorder(Node*& root) { // Preorder : Root Left Right
-    if (root == NULL) {
-        return;
-    }
-    cout << root->data << "  ";
+void preorder(Node* root) {
+    if (root == NULL) { return; }
+    cout << root->data;
     preorder(root->left);
     preorder(root->right);
 }
 
-void postorder(Node*& root) { // Postorder : Left Right Root
-    if (root == NULL) {
-        return;
-    }
+void inorder(Node* root) {
+    if (root == NULL) { return; }
+    inorder(root->left);
+    cout << root->data;
+    inorder(root->right);
+}
+
+void postorder(Node* root) {
+    if (root == NULL) { return; }
     postorder(root->left);
     postorder(root->right);
-    cout << root->data << "  ";
+    cout << root->data;
 }
-
-void inorder(Node*& root) { // Inorder : Left Root Right
-    if (root == NULL) {
-        return;
-    }
-    inorder(root->left);
-    inorder(root->right);
-    cout << root->data << "  ";
-}
-
 
 int main( ) {
-    BT* tree;
-    tree->root = new Node(1);
-    tree->root->left = new Node(2);
-    tree->root->right = new Node(3);
-    tree->root->left->left = new Node(4);
-    tree->root->left->right = new Node(5);
-    tree->root->right->left = new Node(6);
-    tree->root->right->right = new Node(7);
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->left = new Node(6);
+    root->right->right = new Node(7);
 
-    inorder(tree);
+
+    cout << "Postorder : ";
+    postorder(root);
     cout << endl;
 
-    preorder(tree);
+    cout << "Preorder : ";
+    preorder(root);
     cout << endl;
 
-    postorder(tree);
+    cout << "Inorder : ";
+    inorder(root);
     cout << endl;
 
     return 0;
