c2ba54f0d72710a584bf53d0ca151cfb7f1f3fb0
Abhishek Yadav <153166442+43H1-BOI@users.noreply.github.com>
2024-10-05 08:56:52 +0530

    Updated BST

--- Practice/DSA/Data Structure/Binary Tree/Implement.cpp
+++ Practice/DSA/Data Structure/Binary Tree/Implement.cpp
@@ -24,7 +24,7 @@ int Node::count = 0;
 
 void preorder(Node* root) {
     if (root == NULL) { return; }
-    cout << root->data;
+    cout << root->data << " ";
     preorder(root->left);
     preorder(root->right);
 }
@@ -32,7 +32,7 @@ void preorder(Node* root) {
 void inorder(Node* root) {
     if (root == NULL) { return; }
     inorder(root->left);
-    cout << root->data;
+    cout << root->data << " ";
     inorder(root->right);
 }
 
@@ -40,18 +40,117 @@ void postorder(Node* root) {
     if (root == NULL) { return; }
     postorder(root->left);
     postorder(root->right);
-    cout << root->data;
+    cout << root->data << " ";
 }
 
-int main( ) {
-    Node* root = new Node(1);
-    root->left = new Node(2);
-    root->right = new Node(3);
-    root->left->left = new Node(4);
-    root->left->right = new Node(5);
-    root->right->left = new Node(6);
-    root->right->right = new Node(7);
 
+Node* insertByPtr(Node*& root , int val) { // Insert Fun returning Node*
+    if (root == NULL)
+        return new Node(val);
+
+    if (root->data < val)
+        root->right = insertByPtr(root->right , val);
+
+    else if (root->data > val)
+        root->left = insertByPtr(root->left , val);
+
+    return root;
+
+    /* Usage of Above Function in Main()
+
+    // For Fun() returning Node*
+    // root = insertByPtr(root , 33);
+    // root = insertByPtr(root , 33);
+    // root = insertByPtr(root , 60);
+    // root = insertByPtr(root , 60);
+    // root = insertByPtr(root , 10);
+    // root = insertByPtr(root , 10);
+
+    */
+}
+
+/* // Not Working (will fix this later)
+void insert(Node*& root , int val) {
+    if (root == NULL)
+        return;
+
+    if (root->data < val)
+        insert(root->right , val);
+
+    else if (root->data > val)
+        insert(root->left , val);
+
+}
+*/
+
+/*
+Node* insertNode(int val , Node*& root) {
+    Node* newNode = new Node(val);
+    if (root == NULL) {
+        root = newNode;
+        return;
+    }
+    Node* temp = root;
+    while (temp->left != NULL || temp->right != NULL) {
+        if (temp->data > val) {
+
+        }
+        else if (temp->data < val) {
+
+        }
+
+
+
+        if (temp->left == NULL) {
+            if (val >= temp->data) {
+                temp->right = newNode;
+            }
+        }
+        else if (temp->right != NULL) {
+            if (temp->data < val) {
+                temp->left = newNode;
+            }
+        }
+        else {
+            temp = temp->left;
+        }
+
+
+    }
+
+}
+*/
+
+int main( ) {
+
+    Node* root = NULL;
+
+    root = insertByPtr(root , 33);
+    root = insertByPtr(root , 30);
+    root = insertByPtr(root , 60);
+    root = insertByPtr(root , 10);
+    root = insertByPtr(root , 70);
+    root = insertByPtr(root , 1);
+
+/*
+    // For fun() returnning nothing
+    insert(root , 30);
+    insert(root , 30);
+    insert(root , 20);
+    insert(root , 20);
+    insert(root , 50);
+    insert(root , 50);
+*/
+
+/* Starting Insertion(Manual)
+    // Node* root = new Node(1);
+    // root->left = new Node(2);
+    // root->right = new Node(3);
+    // root->left->left = new Node(4);
+    // root->left->right = new Node(5);
+    // root->right->left = new Node(6);
+    // root->right->right = new Node(7);
+*/
 
     cout << "Postorder : ";
     postorder(root);
